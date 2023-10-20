5fbd2f7a3cc36b83cb12c49a2b4434df6732a285
nicobarak <baraknicolas@gmail.com>
2023-10-20 00:46:02 -0300

    Rework Access to API

--- poster_downloader.py
+++ poster_downloader.py
@@ -1,10 +1,14 @@
 import tmdbsimple as tmdb
 import re
+from dotenv import load_dotenv
+import os
 
 
 def get_movie_obj(movie_name, year):
     try:
-        tmdb.API_KEY = "7de1ad8d33f39ee11c34ba41813e0f25"
+        tmdb.API_KEY = os.getenv("TMDB_API_KEY")
+        if tmdb.API_KEY is None:
+            tmdb.API_KEY = TMDB_API_KEY
         search = tmdb.Search()
         search.movie(query=movie_name, year=year)
         if search.total_results <= 0:
